7d3b180af8a39287ebb919cbbe2492a59e4c0f0d
pear-nnnnn2 <108710610+pear-nnnnn2@users.noreply.github.com>
2023-07-13 15:45:17 +0800

    model.py

--- model.py
+++ model.py
@@ -6,15 +6,17 @@ class Model:
         # 这是一个抽象方法，应该在每个子类中实现
         raise NotImplementedError
 
-    def split_data(self, data, test_size):
+    def split_data_K_Fold(self, data, test_size):
+        # 这是一个抽象方法，应该在每个子类中实现
+        raise NotImplementedError
+
+    def split_data_Random(self, data, test_size):
         # 这是一个抽象方法，应该在每个子类中实现
         raise NotImplementedError
 
     def train_data(self, X_train, y_train):
         # 这是一个抽象方法，应该在每个子类中实现
         raise NotImplementedError
-
-
 
 
 
