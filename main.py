1d1e94dd086cd8e4a8724c988883980c52672aff
杨钧堡 <139315657+Mzb830@users.noreply.github.com>
2023-07-14 13:40:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
-
-from linearregression import LinearRegression
-
+from PCA import PCAA
+from K_means_clustering import KMeansClustering
+from K_nearest_neighbors import K_nearest_neighbors
 
 if __name__ == '__main__':
+    # PCA降维算法
     # 1. 创建一个算法模型对象
-    LinearRegression_shili = LinearRegression()
+    PCAA_shili = PCAA()
     # 2. 调用模型对象的方法
-    LinearRegression_shili.test()
+    PCAA_shili.test(test_size=4, dataname="iris", method="kfold", n_components=2)
+    
+    # k均值聚类算法
+    # 1. 创建一个算法模型对象
+    KMeansClustering_shili = KMeansClustering(n_clusters=3)
+    # 2. 调用模型对象的方法
+    KMeansClustering_shili.test()
+    
+    # K最近邻算法
+    # 1. 创建一个算法模型对象
+    K_nearest_neighbors_shili = K_nearest_neighbors()
+    # 2. 调用模型对象的方法
+    K_nearest_neighbors_shili.test(4, "iris", "kfold")
+
+
+
+
+
+
