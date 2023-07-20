b939e49794ae555a9a83c6ce7997c48671f1dae2
pear-nnnnn2 <108710610+pear-nnnnn2@users.noreply.github.com>
2023-07-20 15:03:27 +0800

    Add files via upload

---
+++ js.js
@@ -0,0 +1,20 @@
+function onwine() {
+    var optionfirstleft = document.getElementById("optionfirstleft");  
+    optionfirstleft.checked= true;
+   
+}
+function oniris() {
+    var optionfirstright = document.getElementById("optionfirstright");  
+    optionfirstright.checked= true;
+   
+}
+function onone() {
+    var optionsencondleft = document.getElementById("optionsencondleft");  
+    optionsencondleft.checked= true;
+   
+}
+function onthree() {
+    var optionsencondright = document.getElementById("optionsencondright");  
+    optionsencondright.checked= true;
+   
+}
